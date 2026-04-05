b46a633b32f7777456620cfda1ad38245df6496e
CodeFacts <codefactssolutions@gmail.com>
2026-04-05 12:27:01 +0530

    -changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import  { useState, useEffect, useRef } from 'react';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import AuthModal from './AuthModal';
 import './Navbar.css';
--- src/pages/PropertyDetails.js
+++ src/pages/PropertyDetails.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import { useParams, Link, useNavigate } from 'react-router-dom';
+import  { useState, useEffect } from 'react';
+import { useParams, Link } from 'react-router-dom';
 import PropertyCard from '../components/PropertyCard';
 import './PropertyDetails.css';
 
